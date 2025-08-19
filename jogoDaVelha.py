67640e044a3e279ac9f3bf4d8bb23113b0f6f37e
allan <allanrichard1345@gmail.com>
2025-08-19 01:22:06 -0300

    Otimização do codigo é tratamento de erros

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -12,14 +12,24 @@ def exibeTabuleiro():
         print('-' * 5)
 
 def jogada(linha, coluna):
-
+    if (
+        not 0 <= linha <= 2 or 
+        not 0 <= coluna <= 2 or 
+        tabuleiro[linha][coluna] != ' '
+    ):
+        print('Jogada inválida!')
+        return jogador
     tabuleiro[linha][coluna] = jogador
-    if jogador == 'X':
-        return 'O'
-    else:
-        return 'X'
+    return 'O' if jogador == 'X' else 'X'
 
-jogador = jogada(1,1)
-jogador = jogada(1,2)
-
-exibeTabuleiro()
+while True :
+    print(f'Jogador da vez: {jogador}')
+    try:
+        linha = int(input('Digite a linha: '))
+        coluna = int(input('Digite a coluna: '))
+        jogador = jogada(linha, coluna)
+    except (IndexError):
+        print('Digite valores númericos de 0 e 2')
+    except (ValueError):
+        print('OS valores precisam ser números inteiros ')
+    exibeTabuleiro()
